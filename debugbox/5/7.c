46da89a85016bf113e0220b3ae106d693366068a
LiJiachen <3076451791@qq.com>
2020-06-30 16:40:45 +0800

    ljc debug5 6 to 10

--- debugbox/5/7.c
+++ debugbox/5/7.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-void swap (char **b, char **c) {
-    char* temp=(char *)malloc(31);
-    temp = *b;
-    *b= *c;
-    *c = temp;
+#include<stdlib.h>
+
+void swap (char b[], char c[]) 
+{
+    char temp[25];
+    strcpy(temp, b);
+    strcpy(b, c);
+    strcpy(c, temp);
 }
-int main() {
-    char name[20][31];
-    int i;
-    int j;
+
+int main() 
+{
+    char name[15][25];
+    int i, j;
     int m = 10;
+    //input
     for (i = 0; i < m; i++) {
         scanf("%s", name[i]);    
     }
-    for (j = 0; j < m; j++) {
-        for (i = 0; i <m-j-1; i ++) {
-            if(strcmp(name[i], name[i + 1]) > 0){ 
-                    swap(&name[i], &name[i + 1]);    
+    //sort
+    for (j = 0; j < m - 1; j++) {
+        for (i = 0; i < m - j - 1; i ++) {
+            if (strcmp(name[i], name[i + 1]) > 0) { 
+                    swap(name[i], name[i + 1]);    
             }
         }
     }
+    //output
     for (i = 0; i < m; i++) {
         printf("%s\n", name[i]);    
     }
     return 0;
 }
-
-
