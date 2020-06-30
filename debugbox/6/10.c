075ca36e1975ec6b18e761f22d6a8a14c0b4cbfc
LiJiachen <3076451791@qq.com>
2020-06-30 16:59:41 +0800

    ljc debug6 6 to 10

--- debugbox/6/10.c
+++ debugbox/6/10.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
-int main() {
+
+int main() 
+{
     char pms[3];
-    char change_pms[100][2];
-    int i;
-    i = 0;
+    char change_pms[100][3];
+    int i = 0;
     int j;
-    //int num = 0;
-    int a;//ÎÄ¼ş±¾À´µÄÈ¨ÏŞÖµ
-    a = 0;
-    int b;
-    b = 0;
-    int c;
-    c = 0;
-    
-    //ÊäÈëµ±Ç°ÎÄ¼şÈ¨ÏŞ
-    scanf("%s\n", pms);
-    //printf("%s", pms);
-    //ÊäÈëÌí¼Ó»òÕßÉ¾³ıµÄÎÄ¼şÈ¨ÏŞ
-    while (scanf("%s\n",change_pms[i++]) != EOF) {
-        scanf("%s\n",change_pms[i++]); 
+    int a = 0;   //¿¿¿¿¿¿¿¿
+    int b = 0; 
+    //¿¿¿¿¿¿¿¿
+    scanf("%s", pms);
+    //¿¿¿¿¿¿¿¿¿¿¿¿¿
+    while (scanf("%s", change_pms[i]) != EOF) {
+    	i++;
     }
-    //ÅĞ¶ÏÔ­ÎÄ¼şµÄÈ¨ÏŞ£¬ÓÃÊ®½øÖÆ±íÊ¾
+    j = i;
+    //¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿
     for (i = 0; i < strlen(pms); i++) {
         if (pms[i] == 'r') {
             a += 4;
@@ -30,69 +25,26 @@ int main() {
             a += 1;
         } 
     }
-    //printf("%d\n", a);
-   /* for (i = 0; i <= strlen(change_pms[i]); i++) 
-    {
-        j = 0;
-        if (change_pms[i][j] == '+') 
-        {
-           if (change_pms[i][j + 1] == 'r') 
-           {
-                printf("111");
-               b = b + 4;   
-            } else if (change_pms[i][j + 1] == 'w') 
-           {
-                printf("222");
-                b = b + 2;
-            } else 
-           {
-               printf("222");
-                b = b + 1;
-            }
-        } 
-        /*else {
-            printf("00");
-            if (change_pms[i][j + 1] == 'r') {
-                b = b - 4;   
-            } else if (change_pms[i][j + 1] == 'w') {
-                b = b - 2;
-            } else {
-                b = b - 1;
-            }
-        }*/
     
-    for (i = 0; i < strlen(change_pms[i]); i++) {
-        j = 0;
-        if (change_pms[i][j] == '+') {
-            if (change_pms[i][j + 1] == 'r') 
-           {
-               b += 4;   
-            } else if (change_pms[i][j + 1] == 'w') 
-           {
+    for (i = 0; i < j; i++) {
+        if (change_pms[i][0] == '+') {
+            if (change_pms[i][1] == 'r') {
+                b += 4;   
+            } else if (change_pms[i][1] == 'w') {
                 b += 2;
-            } else 
-           {
+            } else {
                 b += 1;
             }
         } else {
-            if (change_pms[i][j + 1] == 'r') 
-           {
-               b -= 4;   
-            } else if (change_pms[i][j + 1] == 'w') 
-           {
+            if (change_pms[i][1] == 'r') {
+                b -= 4;   
+            } else if (change_pms[i][1] == 'w') {
                 b -= 2;
-            } else 
-           {
+            } else {
                 b -= 1;
             }
         }
     }
-    /*for (i = 0; i < 4; i++) {
-            printf("%c-%c\n",change_pms[i][0], change_pms[i][1]);
-    }*/
-    //printf("%d\n", strlen(change_pms[0]);
-    //printf("%d\n", b);
-    //printf("%d\n", c);
     printf("%d", a + b);
     return 0;
 }
--- debugbox/6/8.c
+++ debugbox/6/8.c
@@ -1,50 +1,49 @@
 #include <stdio.h>
-int main() {
+
+int main() 
+{
     char n;
+    int i;
     int a[3]={0,0,0};
-    for(int i=0;i<3;i++){
+    for (i = 0; i < 3; i++) {
        scanf("%c",&n);
-        if(n == 'r'){
+       if(n == 'r'){
             a[0]=1;
-        }
-        if(n == 'w'){
+       } else if(n == 'w') {
             a[1]=1;
-        }
-        if(n == 'x'){
+       } else if(n == 'x') {
             a[2]=1;
-        }
-        
-    }
-    while (scanf("%c" , &n) !=EOF){
-       if(n == '\n'){
-          continue;
+       } else {
+            break;
        }
-       if(n=='+'){
+    }
+    while (scanf("%c", &n) != EOF) {
+       if (n == '+') {
            scanf("%c" , &n);
-           if(n == 'r'){
-            a[0]=1;
+           if (n == 'r') {
+               a[0]=1;
            }
-           if(n == 'w'){
-            a[1]=1;
+           if (n == 'w') {
+               a[1]=1;
            }
-           if(n == 'x'){
-            a[2]=1;
+           if(n == 'x') {
+               a[2]=1;
            }  
        } 
-        if(n=='-'){
+       if (n == '-') {
            scanf("%c" , &n);
-           if(n == 'r'){
-            a[0]=0;
+           if (n == 'r') {
+               a[0]=0;
            }
-           if(n == 'w'){
-            a[1]=0;
+           if (n == 'w') {
+               a[1]=0;
            }
-           if(n == 'x'){
-            a[2]=0;
+           if (n == 'x') {
+               a[2]=0;
            }  
        } 
     }
-    int o=a[0]*2*2+a[1]*2+a[2];
+    int o = a[0] * 2 * 2 + a[1] * 2 + a[2];
     printf("%d",o);
     return 0;
 }
